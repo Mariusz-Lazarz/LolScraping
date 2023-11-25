424e958c873ceb449d66e52d26fd6a0432960bd8
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-11-25 21:29:44 +0100

    creating route for running a script

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 const puppeteer = require("puppeteer");
-const axios = require("axios");
 
 const scrapeScript = async () => {
-  const browser = await puppeteer.launch({ headless: 'new' });
-  const page = await browser.newPage();
-  await page.goto("https://lolesports.com/schedule?leagues=lec");
-  await page.waitForSelector(".EventDate, .EventMatch");
+  let browser;
+  try {
+    browser = await puppeteer.launch({ headless: "new" });
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
 
-  const data = await page.evaluate(() => {
-    try {
+    await page.goto("https://lolesports.com/schedule?leagues=lec", {
+      waitUntil: "networkidle0",
+    });
+
+    await page.waitForSelector(".EventDate, .EventMatch", { timeout: 5000 });
+
+    const data = await page.evaluate(() => {
       const nodes = Array.from(
         document.querySelectorAll(".EventDate, .EventMatch")
       );
       const results = [];
-
       let currentDate = null;
 
       nodes.forEach((node) => {
         if (node.matches(".EventDate")) {
-          if (node.textContent.startsWith("Yesterday")) {
-            const dateObj = new Date();
-            dateObj.setDate(dateObj.getDate() - 1);
-            const weekday = dateObj.toLocaleString("default", {
-              weekday: "long",
-            });
-            const month = dateObj.toLocaleString("default", { month: "long" });
-            const day = dateObj.getDate();
-            currentDate = `${weekday}-${day} ${month}`;
-          } else {
-            currentDate = node.textContent;
-          }
+          currentDate = node.textContent;
         } else if (currentDate && node.matches(".EventMatch")) {
           const team1 =
             node.querySelector(".team1 span.name")?.textContent || "N/A";
           const team2 =
             node.querySelector(".team2 span.name")?.textContent || "N/A";
           const score = node.querySelector(".score")?.textContent || "N/A";
+
           const existingDateObj = results.find(
             (result) => result.date === currentDate
           );
@@ -49,28 +43,25 @@ const scrapeScript = async () => {
           }
         }
       });
-
       return results;
-    } catch (error) {
-      console.error("Error during page evaluation:", error);
-      return [];
+    });
+
+    if (data.length === 0) {
+      throw new Error("No data found on the page");
     }
-  });
-
-  await browser.close();
-  return data;
-};
-
-const init = async () => {
-  try {
-    const data = await scrapeScript();
-    const response = await axios.put(
-      "https://react-ffef8-default-rtdb.europe-west1.firebasedatabase.app/matches.json",
-      data
-    );
+    console.log(data);
+    return data;
   } catch (error) {
-    console.error(error);
+    throw new Error(`Scraping failed: ${error.message}`);
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
 };
 
-init();
+if (require.main === module) {
+  scrapeScript().catch((error) => console.error(error));
+}
+
+module.exports = scrapeScript;
